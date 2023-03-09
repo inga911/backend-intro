990d0a131585fb88690dea522d1070a2092ff8ca
inga911 <inga.banaityte221@gmail.com>
2023-03-09 06:18:21 +0100

    failu perdavimas

---
+++ 010-php-masyvai/dir-2/file1.php
@@ -0,0 +1,8 @@
+<?php
+
+echo '<h1>F1 START</h1>';
+
+// function a() {}
+
+
+echo '<h1>F1 END</h1>';
---
+++ 010-php-masyvai/inc/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo '<h1>INDEX START</h1>';
+
+
+require __DIR__ .'/../dir2/file1.php';
+require __DIR__ .'/../dir2/file1.php';
+
+
+echo '<h1>INDEX END</h1>';
